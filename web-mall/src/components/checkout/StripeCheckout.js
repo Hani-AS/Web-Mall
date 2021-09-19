67eec9f3dc15d5e431e45b95c31dcc7efa572d17
Hani al Sabti <h.sabti87@gmail.com>
2021-09-19 15:01:17 +0200

    finished stripe checkout handler

---
+++ web-mall/src/components/checkout/StripeCheckout.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useStripe } from "@stripe/react-stripe-js";
+import { CartState } from "../../context/cart/CartContext";
+import { fetchFromApi } from "../../utils/helpers";
+import { Button } from "@mui/material";
+
+export const StripeCheckout = () => {
+  const {
+    state: { cart },
+  } = CartState();
+  const stripe = useStripe();
+
+  const handleCheckOut = async (e) => {
+    const line_items = cart.map((item) => {
+      return {
+        quantity: item.qty,
+        price_data: {
+          currency: "EUR",
+          unit_amount: item.price * 100, // in cents
+          product_data: {
+            name: item.title,
+            description: item.description,
+            images: [item.image],
+          },
+        },
+      };
+    });
+    const response = await fetchFromApi("create-checkout-session", {
+      body: { line_items },
+    });
+    const { sessionId } = response;
+    const { error } = await stripe.redirectToCheckout({
+      sessionId,
+    });
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Button variant="contained" onClick={handleCheckOut}>
+      Proceed to checkout
+    </Button>
+  );
+};
