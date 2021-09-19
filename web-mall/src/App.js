6732cb865880610c32487631262115dc1e03a40e
Hani al Sabti <h.sabti87@gmail.com>
2021-09-19 15:45:20 +0200

    added canceled page

--- web-mall/src/App.js
+++ web-mall/src/App.js
@@ -14,6 +14,7 @@ import { ScrollToTop } from "./utils/ScrollToTop";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { Success } from "./components/checkout/Success";
+import { Canceled } from "./components/checkout/Canceled";
 
 const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
@@ -30,6 +31,9 @@ function App() {
                 <NavBar />
                 <Container>
                   <Switch>
+                    <Route exact path="/canceled">
+                      <Canceled />
+                    </Route>
                     <Route exact path="/success">
                       <Success />
                     </Route>
---
+++ web-mall/src/components/checkout/Canceled.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Canceled = () => {
+  return (
+    <div>
+      <h1>Payment failed</h1>
+      <p>Payment was not successful</p>
+    </div>
+  );
+};
